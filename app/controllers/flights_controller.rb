79042e68cf8e3f68f639dfdfb47875523814cec6
Valdemar <valdemar.kobelis@gmail.com>
2023-10-03 19:07:55 +0300

    Fix bugs with data

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,18 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    if params.include?(:flight)
+      flight_params_shortcuts
+      @flights = Flight.where("flights.arrival_airport_id = ? AND flights.departure_airport_id = ? AND start LIKE ?", @arrival_airport_id, @departure_airport_id, "#{@start}%")
+    else
+      @flights = Flight.all
+    end
+  end
+
+  private
+
+  def flight_params_shortcuts
+    @arrival_airport_id = params[:flight][:arrival_airport_id]
+    @departure_airport_id = params[:flight][:departure_airport_id]
+    @start = params[:flight][:start]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,17 +4,37 @@
 # Examples:
 #
 
-Airport.create([{ code: "ATL" }, { code: "DFW" }, { code: "DEN" }, 
-  { code: "ORD" }, { code: "DXB" }, { code: "LAX" }, { code: "IST" }, 
-  { code: "LHR" }, { code: "DEL" }, { code: "CDG" }
-  ])
+Airport.create([{ code: "ATL", id: 1 }, { code: "DFW", id: 2 }, { code: "DEN", id: 3 }])
 
-Flight.create([{ departure_airport_id: 81, arrival_airport_id: 82, start: DateTime.new(2023, 10, 5, 17, 30, 0), flight_duration: 3.00 }, 
-  { departure_airport_id: 82, arrival_airport_id: 83, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
-  { departure_airport_id: 83, arrival_airport_id: 84, start: DateTime.new(2023, 10, 6, 19, 00, 0), flight_duration: 5.30 },
-  { departure_airport_id: 85, arrival_airport_id: 84, start: DateTime.new(2023, 10, 6, 12, 30, 0), flight_duration: 1.20 },
-  { departure_airport_id: 81, arrival_airport_id: 89, start: DateTime.new(2023, 10, 6, 14, 40, 0), flight_duration: 2.20 },
-  { departure_airport_id: 88, arrival_airport_id: 83, start: DateTime.new(2023, 10, 4, 4, 50, 0), flight_duration: 3.00 },
-  { departure_airport_id: 87, arrival_airport_id: 84, start: DateTime.new(2023, 10, 7, 20, 00, 0), flight_duration: 2.30 },
-  { departure_airport_id: 82, arrival_airport_id: 90, start: DateTime.new(2023, 10, 7, 10, 00, 0), flight_duration: 1.40 }
+Flight.create([{ departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 5, 17, 30, 0), flight_duration: 3.00 }, 
+  { departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 3, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 3, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 1, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 2, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 3, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 3, start: DateTime.new(2023, 10, 5, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 3, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 1, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 2, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 3, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 3, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 2, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 1, start: DateTime.new(2023, 10, 6, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 1, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 2, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 3, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 3, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 2, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 3, arrival_airport_id: 1, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 2, arrival_airport_id: 1, start: DateTime.new(2023, 10, 7, 17, 10, 0), flight_duration: 4.00 },
+  { departure_airport_id: 1, arrival_airport_id: 2, start: DateTime.new(2023, 10, 7, 10, 15, 0), flight_duration: 1.40 }
   ])
