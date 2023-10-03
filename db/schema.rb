7139f40563d17aaaa4db36e23bd8ed0eeee4f598
Valdemar <valdemar.kobelis@gmail.com>
2023-10-03 21:08:17 +0300

    Create Booking and Passanger models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_121005) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_180122) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.integer "passanger_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passanger_id"], name: "index_bookings_on_passanger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -30,5 +39,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_121005) do
     t.index ["{:to_table=>\"airports\"}_id"], name: "index_flights_on_{:to_table=>\"airports\"}_id"
   end
 
+  create_table "passangers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "passangers"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
