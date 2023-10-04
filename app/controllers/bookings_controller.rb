3b38f6329218b20480013bc06289ef258c1be779
Valdemar <valdemar.kobelis@gmail.com>
2023-10-04 09:39:40 +0300

    Create booking show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,11 +9,15 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     if @booking.save
-      redirect_to root_path
+      redirect_to booking_path(@booking.id)
     else
       @flight = Flight.find(params[:booking][:flight_id])
       render :new
     end
+  end
+
+  def show
+    @booking = Booking.includes(:flight).find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :flights
-  resources :bookings, only: [:new, :create]
+  resources :bookings, only: [:new, :create, :show]
   root to: 'flights#index'
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
